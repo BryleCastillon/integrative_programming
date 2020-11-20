a30e26e945a8019408f9777db0a4bf8bebbe0af1
Bryle Castillon <brylecourdey@gmail.com>
2020-11-20 20:33:28 +0800

    index putted

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+class User{
+public $Name;
+public $Username;
+public $Email;
+
+function construct($Name,$Username,$Email){
+    $this->name = $Name;
+    $this->username = $Username;
+    $this->email = $Email;
+}
+
+function addFriend(){
+    echo $this->Name, "Added a Friend.";
+}
+function postStatus(){
+    echo $this->Name, "Posted a Status.";
+}
+}
+
+$me = new User("Bryle  Castillon", "Bwylle", "brylecourdey@gmail.com");
+$me->addFriend();
+echo  "<br>";
+$me->postStatus();
+?>
